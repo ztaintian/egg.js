55d0f2b8283ea3e9618249b8e6e5512440b6a3b3
ztiantian <243752773@qq.com>
2017-04-07 10:01:03 +0800

    egg

---
+++ egg-example/config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1491449460029_6962';
+
+  return config;
+};
